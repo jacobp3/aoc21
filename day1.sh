9a995869bfc3d51f410d46977e1635952dd15ccc
Jacob Pickering <jacobp3@users.noreply.github.com>
2021-12-04 18:40:01 -0800

    Update day1.sh

--- day1.sh
+++ day1.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+last=999999999
+count=0
+while read line; do
+    for word in $line; do
+        if [ $word -ge $last ]; then
+            ((count+=1))
+        fi
+        last=$word
+    done
+done <"day1.txt"
+echo "$count"
