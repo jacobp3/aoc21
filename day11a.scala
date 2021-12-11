f4ec27afee77c6be285304bf5b2712f8f4b7fa23
Jacob Pickering <jacobp3@users.noreply.github.com>
2021-12-11 12:34:19 -0800

    Update day11a.scala

--- day11a.scala
+++ day11a.scala
@@ -6,22 +6,42 @@ object Main {
   def main(args: Array[String]): Unit = {
     var grid = Array[Array[Int]]()
     var flashers = Queue[(Int,Int)]()
+    var result = 0
     val filename = "day11.txt"
+    val days = 100
     for (line <- Source.fromFile(filename).getLines) {
       grid ++= Array(line.toCharArray().map(_.asDigit))
     }
-    for(row <- 0 until grid.length) {
-      for(col <- 0 until grid(row).length) {
-        grid(row)(col) = grid(row)(col) + 1
-        if(grid(row)(col) == 9) {
-          flashers += ((row,col))
+    for(n <- 1 until days) {
+      for(row <- 0 until grid.length) {
+        for(col <- 0 until grid(row).length) {
+          grid(row)(col) = grid(row)(col) + 1
+          if(grid(row)(col) == 9) {
+            flashers += ((row,col))
+          }
+        }
+      }
+      while(!flashers.isEmpty) {
+        var f = flashers.dequeue
+        result++
+        var r = f.row
+      
+        grid(row)(col) = 0
+        for(i <- (row - 1) until (row + 1) {
+          for(j <- (col - 1) until (col + 1)) {
+            if(0 <= i && i < grid.length && 0 <= j && j < grid.length) {
+              val x = grid(i)(j)
+              if(x > 0 && x < 9) {
+                if(x == 8) {
+                  flashers += ((i,j))
+                }
+                grid(i)(j) = x + 1
+              }
+            }
+          }
         }
       }
     }
-    while(!flashers.isEmpty) {
-      f = flashers.dequeue
-      grid(row)(col) = 0
-      //dostuff
-    }
+    println(result)
   }
 }
