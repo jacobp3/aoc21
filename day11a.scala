9dc7031111f219ff335580dc882759eb576e94c7
Jacob Pickering <jacobp3@users.noreply.github.com>
2021-12-11 12:51:44 -0800

    Update day11a.scala

--- day11a.scala
+++ day11a.scala
@@ -12,27 +12,28 @@ object Main {
     for (line <- Source.fromFile(filename).getLines) {
       grid ++= Array(line.toCharArray().map(_.asDigit))
     }
-    for(n <- 1 until days) {
+    for(n <- 0 until days) {
       for(row <- 0 until grid.length) {
         for(col <- 0 until grid(row).length) {
           grid(row)(col) = grid(row)(col) + 1
-          if(grid(row)(col) == 9) {
+          if(grid(row)(col) == 10) {
             flashers += ((row,col))
           }
         }
       }
       while(!flashers.isEmpty) {
         var f = flashers.dequeue
-        result++
-        var r = f.row
+        result = result+1
+        var row = f._1
+        var col = f._2
       
         grid(row)(col) = 0
-        for(i <- (row - 1) until (row + 1) {
-          for(j <- (col - 1) until (col + 1)) {
+        for(i <- (row - 1) to (row + 1)) {
+          for(j <- (col - 1) to (col + 1)) {
             if(0 <= i && i < grid.length && 0 <= j && j < grid.length) {
               val x = grid(i)(j)
-              if(x > 0 && x < 9) {
-                if(x == 8) {
+              if(x > 0 && x < 10) {
+                if(x == 9) {
                   flashers += ((i,j))
                 }
                 grid(i)(j) = x + 1
