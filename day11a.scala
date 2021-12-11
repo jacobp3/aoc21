7c78b0b66e6fa885302014c4fa4b90e27a8d48a5
Jacob Pickering <jacobp3@users.noreply.github.com>
2021-12-11 00:09:23 -0800

    Update day11a.scala

--- day11a.scala
+++ day11a.scala
@@ -1,22 +1,27 @@
 import scala.io.Source
 import scala.collection.immutable
+import scala.collection.mutable.Queue
 
 object Main {
   def main(args: Array[String]): Unit = {
     var grid = Array[Array[Int]]()
+    var flashers = Queue[(Int,Int)]()
     val filename = "day11.txt"
     for (line <- Source.fromFile(filename).getLines) {
       grid ++= Array(line.toCharArray().map(_.asDigit))
     }
-    println(grid(5)(5))
-    grid = grid.map(_.map(x => x+1))
-    println(grid(5)(5))
     for(row <- 0 until grid.length) {
-      for(col <- 0 until row.length) {
-        if(grid(row)(col) >= 9) {
-          println(grid(row)(col))
+      for(col <- 0 until grid(row).length) {
+        grid(row)(col) = grid(row)(col) + 1
+        if(grid(row)(col) == 9) {
+          flashers += ((row,col))
         }
       }
+    }
+    while(!flashers.isEmpty) {
+      f = flashers.dequeue
+      grid(row)(col) = 0
+      //dostuff
     }
   }
 }
